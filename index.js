c2f7869b1a1eaf9c0c7aac1619e1d991f26a7e44
vboxuser <khaoulaaourrath@gmail.com>
2023-09-11 23:29:34 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(source, sought) {
+    console.log("Inside findMatching function"); 
+    return source.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(source, testString) {
+    console.log("Inside fuzzyMatch function");
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(source, soughtName) {
+    console.log("Inside matchName function"); 
+    return source.filter((record) => record.name === soughtName);
+  }
+  
